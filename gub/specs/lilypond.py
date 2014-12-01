7873a41c84b7054ca6b79e753110d29e4228034f
Masamichi Hosoda <trueroad@users.noreply.github.com>
2014-12-01 23:14:16 +0900

    Smaller optimization-disabled region
    On mingw, only lily/skyline.cc disables optimization
    and other files enable.
    
    By this, bad_alloc is prevented.
    And as far as it's possible, the optimization enabled maximally.

--- gub/specs/lilypond.py
+++ gub/specs/lilypond.py
@@ -173,10 +173,8 @@ class LilyPond__mingw (LilyPond):
             'tools::icoutils',
             'mingw-w64-runtime-winpthread-dll',
             ]
+    patches = ['lilypond-lily-skyline-O0.patch']
     python_lib = '%(system_prefix)s/bin/libpython*.dll'
-    configure_flags = (LilyPond.configure_flags
-                       + ' --enable-optimising=off'
-                       )
     make_flags = (LilyPond.make_flags
                   + ' LDFLAGS="%(python_lib)s"'  % locals ())
     # ugh Python hack: C&P Cygwin
