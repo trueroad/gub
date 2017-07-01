05ef2427ed53d50adfe049c0d68bf625231ec44d
Hans Aikema <aikebah@aikebah.net>
2017-07-01 11:01:29 +0200

    www.very-clever.com disappeared - update texi2html URL to its location discovered via http://www.nongnu.org/texi2html/texi2html-old.html

--- gub/specs/texi2html.py
+++ gub/specs/texi2html.py
@@ -2,4 +2,4 @@ from gub import tools
 
 class Texi2html__tools (tools.AutoBuild):
     #source = 'http://lilypond.org/vc/texi2html.git&branch=master'
-    source = 'http://www.very-clever.com/download/nongnu/texi2html/texi2html-1.82.tar.gz'
+    source = 'http://download.savannah.gnu.org/releases/texi2html/texi2html-1.82.tar.gz'
